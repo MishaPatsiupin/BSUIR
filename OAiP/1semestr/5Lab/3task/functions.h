96f26dc88f53de0fd3813e29044eaa22960829c9
Патюпин Михаил, группа 250505 <112729980+MishaPatsiupin@users.noreply.github.com>
2022-11-17 21:07:47 +0300

    Create functions.h

---
+++ OAiP/1semestr/5Lab/3task/functions.h
@@ -0,0 +1,18 @@
+
+#ifndef UNTITLED_FUNTIONS_H
+#define UNTITLED_FUNTIONS_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void inputRowsCols(int *rows);
+int **Matrix(int rows, int cols);
+void printMatrix(int **arr, int rows);
+void input_Matrix(int** arr, int rows, int* cols);
+int cols(int** arr, int row);
+void coppi (int*** arr, int rows);
+
+
+
+#endif //UNTITLED_FUNTIONS_H
