1191cb080cb9fca31745ed152c8822faafe71126
Mishabear9 <112729980+MishaPatsiupin@users.noreply.github.com>
2022-11-24 01:51:13 +0300

    Update fonk.h

--- OAiP/1semestr/6Lab/2task/fonk.h
+++ OAiP/1semestr/6Lab/2task/fonk.h
@@ -9,12 +9,10 @@ void input_rows_cols(int *rows, int *cols);
 int random();
 int **matrix(int rows, int cols);
 void printMatrix(int **arr, int rows, int cols);
-void  sort(int **arr, int rows, int cols);
 int sum_even(int **arr, int row, int cols);
 int* array_characteristic(int **arr, int rows, int cols);
-void merge_sort(int **arr, int i, int j, int *a, int *aux);
-
-
+void merge_sort(int **mas, int *arr, int l, int r);
+void swap(int **arr, int dest, int src);
 
 
 
