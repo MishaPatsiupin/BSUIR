156ca0258a5bfd54647f4e2d4bff5da5a7faf70e
Misha_bear9 <112729980+MishaPatsiupin@users.noreply.github.com>
2024-04-26 00:56:51 +0300

    Add files via upload

---
+++ 4st semestr/OSiSP/PATSIUPIN_OSiSP_prj_2024/front/config.h
@@ -0,0 +1,15 @@
+#ifndef COURSE_03_CONFIG_H
+#define COURSE_03_CONFIG_H
+
+#include <json/json.h>
+#include <fstream>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include "menu.h"
+
+
+
+
+void update_config_file();
+#endif //COURSE_03_CONFIG_H
